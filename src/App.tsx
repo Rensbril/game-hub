c241fd57e908d11b8197c634b87c45f2e4be17cf
Rens Bril <rensbril@gmail.com>
2023-05-04 13:34:49 +0200

    refactioring: created a reusable API client

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import { useEffect, useState } from "react";
-import { Genre } from "./hooks/useGenres";
-import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/useGames";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
 import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import GameGrid from "./components/GameGrid";
+import GameHeading from "./components/GameHeading";
+import GenreList from "./components/GenreList";
+import NavBar from "./components/NavBar";
+import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
 import supabase from "./components/supabaseClient";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/usePlatforms";
 
 export interface GameQuery {
   genre: Genre | null;
