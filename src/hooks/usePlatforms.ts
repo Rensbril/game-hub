635b9e05ec1f48bf9135b9138ebb044552612656
Rens Bril <rensbril@gmail.com>
2023-05-04 16:35:17 +0200

    Fixed bug: implemented infinite scroll

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
@@ -14,7 +13,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: platforms.length, results: platforms },
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    // initialData: { count: platforms.length, results: platforms },
   });
 export default usePlatforms;
